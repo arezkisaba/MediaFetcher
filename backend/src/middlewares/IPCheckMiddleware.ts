4f5a53e55e9b4969d9ad04742033446eec55a748
Arezki Saba <saba.arezki@outlook.com>
2024-06-23 20:44:57 +0200

    Fix IPCheckMiddleware for docker

--- backend/src/middlewares/IPCheckMiddleware.ts
+++ backend/src/middlewares/IPCheckMiddleware.ts
@@ -2,13 +2,12 @@ import { Request, Response, NextFunction } from 'express';
 
 const IPCheckMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const allowedIps = ['::1', '127.0.0.1', 'localhost'];
-    const ip = req.ip;
     const host = req.headers['host']?.split(':')[0];
 
-    if (allowedIps.includes(ip ?? '') && allowedIps.includes(host ?? '')) {
+    if (allowedIps.includes(host ?? '')) {
         next();
     } else {
-        res.status(403).json({ message: 'Forbidden: Access is denied.' });
+        res.status(403).json({ message: 'Forbidden: Access is denied' });
     }
 };
 
